8b1c89803e85559a68f8d4d8358f18cabd17335a
manojkrjoshi <manojkr.joshi@gmail.com>
2021-08-17 16:38:06 +0530

    get vpc by custom accessId and secretKey

--- src/main/java/com/synectiks/aws/main/XformAwsCli.java
+++ src/main/java/com/synectiks/aws/main/XformAwsCli.java
@@ -5,39 +5,31 @@ import com.synectiks.aws.vpc.VpcProcessor;
 import software.amazon.awssdk.regions.Region;
 
 public class XformAwsCli {
-	
+
 	public static void main(String[] a) {
-		
+
 		if (a.length > 0) {
+//			VpcProcessor vp = new VpcProcessor("<access key id>","<secrect key>");
+			VpcProcessor vp = new VpcProcessor();
 			if (!a[0].equalsIgnoreCase("get")) {
 				System.out.println("Invalid command. Example get vpcs [us-east-1] or get vpc <vpc id> [us-east-1]");
 				System.exit(1);
 			}
-			
 			if (a.length == 3 && a[1].equalsIgnoreCase("vpcs")) {
-				VpcProcessor vp = new VpcProcessor();
 				System.out.println(vp.describeEC2Vpcs(Region.of(a[2])));
 				System.exit(0);
-			}else if (a.length == 2 && a[1].equalsIgnoreCase("vpcs")) {
-				VpcProcessor vp = new VpcProcessor();
+			} else if (a.length == 2 && a[1].equalsIgnoreCase("vpcs")) {
 				System.out.println(vp.describeEC2Vpcs(null));
 				System.exit(0);
-			}else if (a.length == 4 && a[1].equalsIgnoreCase("vpc")) {
+			} else if (a.length == 4 && a[1].equalsIgnoreCase("vpc")) {
 				String vpcId = a[2];
-				VpcProcessor vp = new VpcProcessor();
 				System.out.println(vp.describeEC2VpcById(Region.of(a[3]), vpcId));
 				System.exit(0);
-			}else if (a.length == 3 && a[1].equalsIgnoreCase("vpc")) {
+			} else if (a.length == 3 && a[1].equalsIgnoreCase("vpc")) {
 				String vpcId = a[2];
-				VpcProcessor vp = new VpcProcessor();
 				System.out.println(vp.describeEC2VpcById(null, vpcId));
 				System.exit(0);
-			}
-//			else {
-//					System.out.println("invalid command please enter vpcId for example get vpc <vpc_id> [us-east-1]");
-//					System.exit(1);
-//				}
-			 else {
+			} else {
 				System.out.println(
 						"Invalid command Please enter \n 1). get vpcs [us-east-1] or to get all vpc details \n 2). get vpc <vpc_id> [us-east-1] ");
 				System.exit(1);
@@ -49,7 +41,5 @@ public class XformAwsCli {
 		}
 
 	}
-
-	
 
 }
